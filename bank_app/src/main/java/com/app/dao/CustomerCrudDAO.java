592609309ece317eecea7326c796ae7b2fe9a4f0
Juan <juan231f@yahoo.com>
2021-01-03 00:32:15 -0500

    added dao and impl

--- bank_app/src/main/java/com/app/dao/CustomerCrudDAO.java
+++ bank_app/src/main/java/com/app/dao/CustomerCrudDAO.java
@@ -7,6 +7,6 @@ import com.app.exception.BusinessException;
 
 public interface CustomerCrudDAO {
 	public int createCustomer(Customer customer) throws BusinessException;
-	public Customer getCustomerByID(int customer_id);
-	public List<Customer> getAllCustomers();
+	public Customer getCustomerById(int customer_id) throws BusinessException;
+	public List<Customer> getAllCustomers() throws BusinessException;
 }
--- bank_app/src/main/java/com/app/main/Main.java
+++ bank_app/src/main/java/com/app/main/Main.java
@@ -1,7 +1,8 @@
 package com.app.main;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
-import org.junit.jupiter.api.Test;
 
 import com.app.bank.Customer;
 import com.app.dao.CustomerCrudDAO;
@@ -13,20 +14,42 @@ public class Main {
 	private static Logger log=Logger.getLogger(Main.class);
 	
 	public static void main(String[] args) {
-		log.info("Hello");
 		
 		CustomerCrudDAO dao = new CustomerCrudDAOImpl();
 		
-
+/*
 		Customer c = new Customer(4, "Fabidaly", "Valera", "12/4/2006", "F", "111-11-1111", "111 111 111", "313 Gates Place", "1E", "Bronx", "NY", "86753", "Fabidaly@gmail.com", "342423333");
-
 		try {
 			if(dao.createCustomer(c)!=0) {
-				log.info("Created Successfully");
+				log.info("\nCreated Successfully");
 			}
 		} catch(BusinessException e) {
-			System.out.println(e.getMessage());
+			log.error(e.getMessage());
 		}
+*/
+/*		
+		try {
+			Customer customer=dao.getCustomerById(1);
+			if(customer!=null) {
+				log.info("\nDetails of customer with id of " + customer.getCustomer_id());
+				log.info(customer);
+			}
+		} catch (BusinessException e) {
+			log.error(e.getMessage());
+		};
+*/
+		try {
+			List<Customer> customerList=dao.getAllCustomers();
+			if(customerList!=null && customerList.size()!=0) {
+				log.info("\nFound " +customerList.size()+ " customers in the DB...");
+				for(Customer c:customerList) {
+					log.info(c);
+				}
+			}
+		} catch (BusinessException e) {
+			log.error(e.getMessage());
+		}
+		
 		
 	}
 }
