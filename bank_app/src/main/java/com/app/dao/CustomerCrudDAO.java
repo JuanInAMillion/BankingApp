49c8f71bc7532bca3b147e002c6eb365bb22a90f
Juan <juan231f@yahoo.com>
2021-01-10 10:40:23 -0500

    fresh restart

--- bank_app/src/main/java/com/app/dao/CustomerCrudDAO.java
+++
@@ -1,8 +0,0 @@
-package com.app.dao;
-
-import com.bank.exception.BusinessException;
-import com.bank.model.Customer;
-
-public interface CustomerCrudDAO {
-	public int createCustomer(Customer customer) throws BusinessException;
-}
--- bank_app/src/main/java/com/app/dao/CustomerSearchDAO.java
+++
@@ -1,8 +0,0 @@
-package com.app.dao;
-
-import com.bank.exception.BusinessException;
-import com.bank.model.Customer;
-
-public interface CustomerSearchDAO {
-	public Customer getCustomerById(int customer_id) throws BusinessException;
-}
